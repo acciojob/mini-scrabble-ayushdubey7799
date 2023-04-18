ab6b5239e2b169026fc1f3202b24fb00161b7111
ayushdubey7799 <113593098+ayushdubey7799@users.noreply.github.com>
2023-04-18 13:33:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let input = document.getElementById('evaluatedText');
+let letterCount = document.getElementById('letterCount');
+
+input.addEventListener('input', function(){
+    letterCount.textContent = this.value.length;
+});
